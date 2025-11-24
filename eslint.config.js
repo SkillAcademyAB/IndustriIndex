765f4a28ed5bf3ebb4a17c6381e133726b0609ba
Leo <smookefish@gmail.com>
2025-11-24 13:18:31 +0100

    Lägg till .gitignore och uppdatera ESLint-konfiguration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 import js from "@eslint/js"
 import prettier from "eslint-config-prettier"
-//import { addUnderscore } from './custom-rules.js'
-//import { underscoreParams } from './simple-fixer.js'
 
 //Custom rule för underscore med egna meddelanden
 const underscoreRule = {
@@ -9,35 +7,33 @@ const underscoreRule = {
         fixable: "code",
         type: "suggestion",
         messages: {
-            missingUnderscore: "Parameters must start with an underscore. Example: _parameterName",
+            missingUnderscore:
+                "Parameters must start with an underscore. Example: _parameterName"
         }
     },
-    create(_context) 
-    {
+    create(_context) {
         return {
-            FunctionDeclaration(_node) 
-            {
+            FunctionDeclaration(_node) {
                 processParams(_node.params, _context)
             },
-            ArrowFunctionExpression(_node) 
-            {
+            ArrowFunctionExpression(_node) {
                 processParams(_node.params, _context)
             },
-            FunctionExpression(_node) 
-            {
+            FunctionExpression(_node) {
                 processParams(_node.params, _context)
             }
         }
-        
-        function processParams(_params, _context) 
-        {
-            _params.forEach(_param => 
-            {
-                if(_param.type === 'Identifier' && !_param.name.startsWith('_'))
-                {
+
+        function processParams(_params, _context) {
+            _params.forEach((_param) => {
+                if (
+                    _param.type === "Identifier" &&
+                    !_param.name.startsWith("_")
+                ) {
                     _context.report({
                         node: _param,
-                        messageId: "missingUnderscore",
+                        messageId: "missingUnderscore"
+                        //Parameter-prefix med underscore (manuellt fixa)
                         /*fix(_fixer) 
                         {
                             return _fixer.replaceText(_param, `_${_param.name}`)
@@ -57,17 +53,17 @@ export const customBraceStyle = {
             wrongBraceStyle: "Curly braces must be on a new line (Allman style)"
         }
     },
-    create(_context) 
-    {
+    create(_context) {
         return {
-            BlockStatement(_node) 
-            {
+            BlockStatement(_node) {
                 const sourceCode = _context.getSourceCode()
                 const openingBrace = sourceCode.getFirstToken(_node)
                 const previousToken = sourceCode.getTokenBefore(openingBrace)
-                
-                if (previousToken && previousToken.loc.end.line === openingBrace.loc.start.line) 
-                {
+
+                if (
+                    previousToken &&
+                    previousToken.loc.end.line === openingBrace.loc.start.line
+                ) {
                     _context.report({
                         node: openingBrace,
                         messageId: "wrongBraceStyle"
@@ -94,12 +90,26 @@ export default [
         ]
     },
 
+    //CONFIG FILES - INGA underscore regler här!
+    {
+        files: ["**/eslint.config.js"],
+        rules: {
+            //"id-match": "off",         //--> Stäng av underscore regeln för config filer
+            "no-unused-vars": "off", //Stäng av unused vars
+            "prefer-const": "off", //Stäng av prefer-const
+            "no-var": "off", //Stäng av no-var
+            "custom/underscore-rule": "off",
+            "custom/brace-style": "off"
+        }
+    },
+
     //Grundkonfiguration
     js.configs.recommended,
-    
+
     //Prettier config (måste vara sist)
     prettier,
-    
+
+    //--> underscore regler aktiverade här!
     {
         files: ["**/*.js"],
         languageOptions: {
@@ -110,14 +120,10 @@ export default [
                 es2021: true
             }
         },
-        plugins: {
-            //Prettier plugin behövs inte längre i nyare versioner
-            //när vi använder eslint-config-prettier
-        },
         rules: {
             //Måsvingar på ny rad (Allman-style)
             "brace-style": ["error", "allman"],
-            
+
             //Parameter-prefix med underscore (Automatiskt fixa)
             //"custom-rules/add-underscore": "error",
             //Använd vår custom rule istället för id-match
@@ -125,25 +131,29 @@ export default [
             "custom/brace-style": "error",
 
             //Parameter-prefix med underscore (manuellt fixa)
-            "id-match": ["error", "^_[a-zA-Z][a-zA-Z0-9]*$", {
+            //ID-MATCH STÄNGS AV - vill inte ha underscore på variabler!
+            /*"id-match": ["error", "^_[a-zA-Z][a-zA-Z0-9]*$", {
                 properties: false, 
-                onlyDeclarations: false,
+                onlyDeclarations: true, //false,
                 ignoreDestructuring: true,
                 //message: "Parameters must start with an underscore. Example: _parameterName"
-            }],
-        
+            }],*/
+
             //Ytterligare bra regler
-            "no-unused-vars": ["error", { 
-                args: "all", 
-                //argsIgnorePattern: "^_",
-                //varsIgnorePattern: "^_", 
-                //message: "Unused variables must start with underscore or be removed"
-            }],
-            "prefer-const": "error", //message: "Use const instead of let for variables that don't get reassigned" 
-            "no-var": "error" //message: "Use let or const instead of var" 
+            "no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_"
+                    //message: "Unused variables must start with underscore or be removed"
+                }
+            ],
+            "prefer-const": "error", //message: "Use const instead of let for variables that don't get reassigned"
+            "no-var": "error" //message: "Use let or const instead of var"
         },
         plugins: {
-            "custom": {
+            custom: {
                 rules: {
                     "underscore-rule": underscoreRule,
                     "brace-style": customBraceStyle
